52ba8b0a11b7afb92ff4cc264e3ef28bc4f27a85
JackMars <jackmars@example.com>
2026-04-06 17:10:56 +0300

    feat: add services data layer with 8 service categories

---
+++ src/data/services/index.ts
@@ -0,0 +1,48 @@
+export interface SubService {
+  title: string;
+  description: string;
+}
+
+export interface ServiceFAQ {
+  question: string;
+  answer: string;
+}
+
+export interface Service {
+  slug: string;
+  title: string;
+  shortTitle: string;
+  description: string;
+  metaDescription: string;
+  icon: string;
+  isPrimary: boolean;
+  subServices: SubService[];
+  faq: ServiceFAQ[];
+}
+
+import { simejniSpravy } from "./simejni-spravy";
+import { zhytloviSuperechky } from "./zhytlovi-superechky";
+import { mobilizatsiya } from "./mobilizatsiya";
+import { kredytyTaBorhy } from "./kredyty-ta-borhy";
+import { mihratsijnePravo } from "./mihratsijne-pravo";
+import { korporatyvnePravo } from "./korporatyvne-pravo";
+import { zemelneTaAhrarnePravo } from "./zemelne-ta-ahrarne-pravo";
+import { administratyvniSpravy } from "./administratyvni-spravy";
+
+export const services: Service[] = [
+  simejniSpravy,
+  zhytloviSuperechky,
+  mobilizatsiya,
+  kredytyTaBorhy,
+  mihratsijnePravo,
+  korporatyvnePravo,
+  zemelneTaAhrarnePravo,
+  administratyvniSpravy,
+];
+
+export const primaryServices = services.filter((s) => s.isPrimary);
+export const secondaryServices = services.filter((s) => !s.isPrimary);
+
+export function getServiceBySlug(slug: string): Service | undefined {
+  return services.find((s) => s.slug === slug);
+}
